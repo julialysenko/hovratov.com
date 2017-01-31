185dcf5b8ac9b349b9fbb15d51c80e62c3a14340
yulia <yusenkanya@gmail.com>
2017-01-31 09:09:08 +0200

    update

--- scripts/main.js
+++ scripts/main.js
@@ -18,6 +18,35 @@ $(document).on('click', '.head-arrow-down', function (e) {
 
         $body.animate({scrollTop: pos});
 });
+/*
+// image safe
+
+var message = "";
+
+function clickIE() {
+    if (document.all) {
+        (message);
+        return false;
+    }
+}
+
+function clickNS(e) {
+    if (document.layers || (document.getElementById && !document.all)) {
+        if (e.which == 2) {
+            (message);
+            return false;
+        }
+    }
+}
+if (document.layers) {
+    document.captureEvents(Event.MOUSEDOWN);
+    document.onmousedown = clickNS;
+} else {
+    document.onmouseup = clickNS;
+    document.oncontextmenu = clickIE;
+}
+document.oncontextmenu = new Function("return false");
+*/
 
 // textarea
 window.autosize && autosize(document.querySelectorAll('textarea'));
